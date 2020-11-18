771f6aec9cd7ed9911439ffd8216643e588e7733
Rookie001MC <nguyenquocthang2004@gmail.com>
2020-11-18 20:33:35 +0700

    ...

--- Django_Blog/settings.py
+++ Django_Blog/settings.py
@@ -27,7 +27,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY'] # SECURITY WARNING: don't run with debug turned on in production!
+SECRET_KEY = os.environ['SECRET_KEY']
+ # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['https://rookiessecretsanctuary.herokuapp.com']
